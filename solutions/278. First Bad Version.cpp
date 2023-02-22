9ed520788b83d343d96bcce105bf9c265ada34c1
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2023-02-22 15:28:24 +0530

    3 ms

---
+++ solutions/278. First Bad Version.cpp
@@ -0,0 +1,21 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+​
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int l=1;
+        int r=n;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(isBadVersion(mid)==false){
+                l=mid+1;
+            }
+            if(isBadVersion(mid)==true){
+                r=mid-1;
+            }
+        }
+        return l;
+        
+    }
+};
