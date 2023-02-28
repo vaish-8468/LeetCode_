2772babf581dbcb475afe39ef3f12bd9390cede6
Vartika Vaish <2020uee0139@iitjammu.ac.in>
2023-03-01 00:57:23 +0530

    6 ms

--- solutions/299. Bulls and Cows.cpp
+++ solutions/299. Bulls and Cows.cpp
@@ -6,10 +6,13 @@ public:
         int bulls=0;
         int cows=0;
         for(int i=0;i<secret.length();i++){
+            //for secret string
             if(secret[i]==guess[i]){
                 bulls++;
+                //increases the bull count if digits are at their correct place
             }
             else{
+                //otherwise keep a record of frequency of digits which are at their incorrect position
                 if(sp.find(secret[i])!=sp.end()){
                 sp[secret[i]]++;
             }
@@ -17,13 +20,16 @@ public:
                 sp[secret[i]]=1;
             }
                 
-        }
+        }
       }
         for(int i=0;i<guess.length();i++){
+            //for guess string
             if(secret[i]!=guess[i])
+                //if unequal digits are found at a particular position
             {
             if(sp.find(guess[i])!=sp.end()){
                 cows++;
+                //if they already exist in the map, we simply remove them if the frequency is 1 otherwise we decrease the frequency
                 if(sp[guess[i]]==1){
                     sp.erase(guess[i]);
                 }
